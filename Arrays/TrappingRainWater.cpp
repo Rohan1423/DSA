acc32f4356b0dfac0abebf0f71546330ebcd77b4
Rohan Chauhan <chauhanrohan607@gmail.com>
2026-01-13 23:59:09 +0530

    Given an array height[] where each element represents the height of a bar, calculate how much rain water can be trapped between the bars. (Better Approach)

--- Arrays/TrappingRainWater.cpp
+++ Arrays/TrappingRainWater.cpp
@@ -17,8 +17,25 @@ int waterStored(int arr[],int n){ // Brute Force Approach  T.C O(n x n)  S.C O(1
     return waterTrapped;
     
 }
+
+int waterTrapped(int arr[],int n){ // Prefix & Suffix Arrays (Better) T.C O(n). S.C O(n)
+    int waterCap=0;
+    int leftMax[n];int rightMax[n];
+    leftMax[0]=arr[0];
+    for(int i=1; i<n; i++){
+        leftMax[i]= max(leftMax[i-1],arr[i]);  // storing leftMax for every index in array
+    }
+    rightMax[n-1]=arr[n-1];
+    for(int i=n-2;i>=0;i--){
+        rightMax[i]=max(rightMax[i+1],arr[i]); // storing rightMax for every index in array
+    }
+    for(int i=0; i<n; i++){
+        waterCap += min(leftMax[i],rightMax[i])-arr[i];
+    }
+    return waterCap;
+}
 int main (){
-    int arr[]={4,2,0,3,3,5};
+    int arr[]={4,2,0,3,2,5};
     int n=6;
     cout<<waterStored(arr,n)<<endl;
 }
