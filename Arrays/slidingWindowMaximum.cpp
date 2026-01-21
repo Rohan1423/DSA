1902939a6538ed60e02cbc7e7d3f60a1eba50f68
Rohan Chauhan <chauhanrohan607@gmail.com>
2026-01-21 23:59:34 +0530

    Sliding Window Maximum

--- Arrays/slidingWindowMaximum.cpp
+++ Arrays/slidingWindowMaximum.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void slidingWindowMax(int arr[],int n,int k){  // Brute Force  TC: O(n * k), SC: O(1)
@@ -11,9 +12,30 @@ void slidingWindowMax(int arr[],int n,int k){  // Brute Force  TC: O(n * k), SC:
     }
 }
 
+void slidingWindowMaxUsingDeque(int arr[],int n, int k){  // Optimal Approach (Using Deque) TC: O(n), SC: O(k)
+    deque<int>dq;
+    vector<int>res;
+    for(int i=0;i<k;i++){
+        while(dq.size()>0 && arr[dq.back()]<=arr[i]){  // remove if last pushed element is smaller than current element
+            dq.pop_back();
+        }
+        dq.push_back(i);
+    }
+    for(int i=k; i<n; i++){
+        while(dq.size()>0 && arr[dq.front()]<=i-k){  // remove if deque has element which is not in current window
+            dq.pop_front();
+        }
+        while(dq.size()>0 && arr[dq.back()] <=arr[i]){
+            dq.pop_back();
+        }
+        dq.push_back(i);
+    }
+    res.push_back(arr[dq.front()]);
+}
+
 int main(){
     int arr[] = {1, 3, -1, -3, 5, 3, 6, 7};
     int n = 8;
     int k = 3;
-    slidingWindowMax(arr,n,k);
+    slidingWindowMaxUsingDeque(arr,n,k);
 }
