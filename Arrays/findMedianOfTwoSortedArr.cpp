6e6bc236aee95381d6aded5e95aba0522fcfb4b9
Rohan Chauhan <chauhanrohan607@gmail.com>
2026-01-27 22:08:22 +0530

    Given two sorted arrays nums1 and nums2 of sizes n and m, find the median of the two sorted arrays.(Better Approach)

--- Arrays/findMedianOfTwoSortedArr.cpp
+++ Arrays/findMedianOfTwoSortedArr.cpp
@@ -24,6 +24,35 @@ double findMedian(int a[], int n, int b[], int m) {  // Brute Force T.C O(n+m) S
     else
         return (merged[total / 2 - 1] + merged[total / 2]) / 2.0;
 }
+
+double findMedianBetter(int a[], int n, int b[],int m){  // Better (Two pointers, no extra array)  T.C O(n+m) S.C O(1)
+    int total = n+m;
+    int mid1 = (total-1)/2;
+    int mid2 = total/2;
+    int prev=0;
+    int curr=0;
+    int count=0;
+    int i=0;
+    int j=0;
+    while(count<=mid2){
+        prev=curr;
+        if(i<n && (j>=m || a[i]<=b[j])){
+            curr=a[i];
+            i++;
+        }
+        else{
+            curr=b[j];
+            j++;
+        }
+        count++;
+    }
+    if(total%2==1){
+        return curr;
+    }
+    else{
+        return (prev+curr)/2.0;
+    }
+}
 int main() {
     int a[] = {1, 3, 5};
     int b[] = {2, 4, 6};
@@ -31,6 +60,6 @@ int main() {
     int n = 3;
     int m = 3;
 
-    cout << "Median = " << findMedian(a, n, b, m);
+    cout << "Median = " << findMedianBetter(a, n, b, m);
     return 0;
 }
